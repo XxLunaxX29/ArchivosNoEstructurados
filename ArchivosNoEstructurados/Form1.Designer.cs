cb4390b9b79b8f7c834d22a55373c202e26e8efa
XxLunaxX <josellunag2909@gmail.com>
2026-02-22 19:53:56 -0600

    modificacion

--- ArchivosNoEstructurados/Form1.Designer.cs
+++ ArchivosNoEstructurados/Form1.Designer.cs
@@ -44,7 +44,7 @@
             // 
             // btnAbrir
             // 
-            btnAbrir.Location = new Point(164, 43);
+            btnAbrir.Location = new Point(534, 19);
             btnAbrir.Name = "btnAbrir";
             btnAbrir.Size = new Size(94, 29);
             btnAbrir.TabIndex = 0;
@@ -54,7 +54,7 @@
             // 
             // btnCrear
             // 
-            btnCrear.Location = new Point(164, 8);
+            btnCrear.Location = new Point(422, 19);
             btnCrear.Name = "btnCrear";
             btnCrear.Size = new Size(94, 29);
             btnCrear.TabIndex = 1;
@@ -64,7 +64,7 @@
             // 
             // btnEliminar
             // 
-            btnEliminar.Location = new Point(276, 43);
+            btnEliminar.Location = new Point(648, 59);
             btnEliminar.Name = "btnEliminar";
             btnEliminar.Size = new Size(94, 29);
             btnEliminar.TabIndex = 2;
@@ -74,7 +74,7 @@
             // 
             // btnAgregar
             // 
-            btnAgregar.Location = new Point(276, 8);
+            btnAgregar.Location = new Point(648, 19);
             btnAgregar.Name = "btnAgregar";
             btnAgregar.Size = new Size(94, 29);
             btnAgregar.TabIndex = 3;
@@ -84,7 +84,7 @@
             // 
             // btnBuscar
             // 
-            btnBuscar.Location = new Point(390, 12);
+            btnBuscar.Location = new Point(310, 18);
             btnBuscar.Name = "btnBuscar";
             btnBuscar.Size = new Size(94, 29);
             btnBuscar.TabIndex = 4;
@@ -94,7 +94,7 @@
             // 
             // Guardar
             // 
-            Guardar.Location = new Point(390, 43);
+            Guardar.Location = new Point(534, 59);
             Guardar.Name = "Guardar";
             Guardar.Size = new Size(94, 29);
             Guardar.TabIndex = 5;
@@ -113,35 +113,35 @@
             // 
             // txtBuscar
             // 
-            txtBuscar.Location = new Point(80, 77);
+            txtBuscar.Location = new Point(82, 19);
             txtBuscar.Name = "txtBuscar";
-            txtBuscar.Size = new Size(125, 27);
+            txtBuscar.Size = new Size(190, 27);
             txtBuscar.TabIndex = 7;
             // 
             // txtEntrada
             // 
-            txtEntrada.Location = new Point(95, 104);
+            txtEntrada.Location = new Point(82, 56);
             txtEntrada.Name = "txtEntrada";
-            txtEntrada.Size = new Size(125, 27);
+            txtEntrada.Size = new Size(190, 27);
             txtEntrada.TabIndex = 8;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(0, 84);
+            label1.Location = new Point(24, 23);
             label1.Name = "label1";
-            label1.Size = new Size(50, 20);
+            label1.Size = new Size(52, 20);
             label1.TabIndex = 9;
-            label1.Text = "label1";
+            label1.Text = "Buscar";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(24, 104);
+            label2.Location = new Point(26, 63);
             label2.Name = "label2";
-            label2.Size = new Size(50, 20);
+            label2.Size = new Size(48, 20);
             label2.TabIndex = 10;
-            label2.Text = "label2";
+            label2.Text = "Datos";
             // 
             // Form1
             // 
